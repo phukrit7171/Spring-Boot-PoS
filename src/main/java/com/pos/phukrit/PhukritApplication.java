095e612954efa625a0b1804116dc183edddd82bf
Mr.Phukrit Kittinontana <phukrit7171@gmail.com>
2025-08-20 18:33:36 +0700

    fix : main app typo

--- src/main/java/com/pos/phukrit/PhukritApplication.java
+++ src/main/java/com/pos/phukrit/PhukritApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 // Assuming your @Entity classes are in a 'model' or 'entity' sub-package
-@EntityScan(basePackages = "com.pos.phukrit.model")
+@EntityScan(basePackages = "com.pos.phukrit.models")
 public class PhukritApplication {
 
     public static void main(String[] args) {
